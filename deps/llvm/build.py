cf42761fa28b8cdc5f68831569ab79914f0e114d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 01:31:45 +0000

    Adding tarfile member sanitization to extractall()

--- deps/llvm/build.py
+++ deps/llvm/build.py
@@ -57,7 +57,26 @@ def get_llvm(url, dest):
 
     print("Extract llvm ...")
     with tarfile.open(str(base_name), 'r|xz') as tar:
-        tar.extractall(str(dest))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, str(dest))
 
 
 def build_llvm(llvm_dir, build_dir, arch, platform, arch_opt=None):
